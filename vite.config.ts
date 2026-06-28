89b4e5c791c82566362b27be25718cba8cfb9408
emyllecontas-a11y <emyllecontas@gmail.com>
2026-06-28 19:35:57 -0300

    Corrige base path para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/revisa-flash/', // 🔥 ESSA É A LINHA QUE FALTAVA
   builder: 'rollup',
   optimizeDeps: {
     exclude: ['broadcast-channel'],
